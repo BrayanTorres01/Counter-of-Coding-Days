3ecd35867fcee99bc5944578aa35eac103570455
Brayan  Torres <brayantorres@Brayans-MacBook-Pro.local>
2024-01-03 16:18:09 -0800

    Implemented the save button functionality and incorporated a new image. This project is now completed for now. I do plan to add a database later on.

--- index.js
+++ index.js
@@ -5,10 +5,12 @@
 // JS letting us modify html
 let countEl = document.getElementById("count-el")
 let countRestart = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 // let or var allows you to form variables
 // intialize the count as 0
 let count = 0
+
 
 // using the increment function that I initialized in my html as an attr
 // listen for clicks on the increment button
@@ -19,12 +21,23 @@ function increment() {
     countEl.innerText = count;
 }
 
+// allows us to restart the count of days when the buttton is clicked
 function restart() {
     count = 0
     countRestart.innerText = count;
 }
 
+// allows us to save our streak fo days
 function save() {
+        saveEl.innerText += " " + count + " - ";
+   
 
+    // We can also do it this way
+    // let countStr = count + " - "
+    // saveEl.textContent += countStr
+
+    // I can also make it count from zero if the save button is pressed
+    // count = 0;
+    // countEl.innerText = count;
 }
 
